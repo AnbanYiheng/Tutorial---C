650fe55ef1669f96a10c909c44ef5f7b98c13d9a
AnbanYiheng <75653446+AnbanYiheng@users.noreply.github.com>
2020-12-11 18:32:16 +0800

    Update api.c

--- api.c
+++ api.c
@@ -1,21 +1,27 @@
-#include "api.h"
-
-int check(const char *data, int len)
-{
-  char buf[1024];
-  memset(buf, 0, 1024);
-  strncpy(buf, data, len); // BUG - This incorrectly uses length of src, not dst
-
-  // do something to ensure this isn't optimized away
-  int buflen = strlen(buf);
-  for (int i = 0; i <= ((buflen % 2 == 1) ? buflen - 1 : buflen) / 2; i++)
-  {
-    if (buf[i] != buf[buflen - 1 - i])
-    {
-      printf("not palindrome\n");
-      return 0;
-    }
-  }
-  printf("congrats!!palindrome!!\n");
-  return 0;
+#include "api.h" 
+ 
+int check(const char *data, int len) 
+{ 
+  if(len >= 1024)return 0;
+  char buf[1024]; 
+  memset(buf, 0, 1024); 
+  strncpy(buf, data, len);
+ 
+  int buflen = strlen(buf); 
+  
+  //异常的业务逻辑
+  if (size > 0 && data[0] == 'a')
+    if (size > 1 && data[1] == 'n')
+        if (size > 2 && data[2] == 'b')
+            if(size > 3 && data[3] == 'a')
+                if(size > 4 && data[4] == 'n')
+                    if(size > 5 && data[5] == '6')
+                        if(size > 6 && data[6] == '6')
+                            if(size > 7 && data[7] == '6')
+                            {
+                                printf("bug here\n");
+                                int* p = nullptr;  // bug
+                                *p = 23;
+                            }              
+  return 0; 
 }
