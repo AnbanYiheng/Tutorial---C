f319fa2329206fae42ed123f61988f4ed3685a13
AnbanYiheng <75653446+AnbanYiheng@users.noreply.github.com>
2020-12-17 13:34:57 +0800

    Update api.c

--- api.c
+++ api.c
@@ -3,22 +3,57 @@
  
 int check(const char *data, int len) 
 { 
-  if(len >= 1024)return 0;
-  printf("run\n"); 
-  
+  if(len < 4)return 1;
+
+  int cnt = 0;
   //异常的业务逻辑
-  if (len > 0 && data[0] == 'a')
-    if (len > 1 && data[1] == 'n')
-        if (len > 2 && data[2] == 'b')
-            if(len > 3 && data[3] == 'a')
-                if(len > 4 && data[4] == 'n')
-                    if(len > 5 && data[5] == '6')
-                        if(len > 6 && data[6] == '6')
-                            if(len > 7 && data[7] == '6')
-                            {
-                                printf("bug here\n");
-                                int* p = NULL;  // bug
-                                *p = 23;
-                            }                                  
+  if (data[0] == 'x') { cnt++; }
+  if (data[1] == 'y') { cnt++; }
+  if (data[2] == 'z') { cnt++; }
+
+  if (cnt >= 3) {
+    switch (data[3]) {
+      case '0': {
+        // segv
+        int *p = NULL; *p = 123;
+        break;
+      }
+      case '1': {
+        // stack-buffer-underflow
+        int* p = &cnt - 32; for (int i = 0; i < 32; i++) { *(p + i) = 0; }
+        break;
+      }
+      case '2': {
+        // stack-buffer-overflow 
+        int* p = &cnt + 32; for (int i = 0; i < 32; i++) { *(p - i) = 0; }
+        break;
+      }
+      case '3': {
+        // bad-free
+        int *p = &cnt; free(p);
+        break;
+      }
+      case '4': {
+        // double-free
+        int* p = malloc(sizeof(int)); free(p); free(p);
+        break;
+      }
+      case '5': {
+        // heap-use-after-free
+        int* p = malloc(sizeof(int)); free(p); *p = 123;
+        break;
+      }
+      case '6': {
+        // heap-buffer-overflow
+        int* p = malloc(8 * sizeof(int)); for (int i = 0; i < 32; i++) { *(p + i) = 0; }
+        break;
+      }
+      case '7': {
+        // fpe
+        int x = 0; int y = 123 / x;
+        break;
+      }
+    }
+  }                             
   return 0; 
 }
